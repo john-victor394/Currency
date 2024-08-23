8a048132eaf51b1e313525384e9baa1839a29b5d
john-victor394 <137208505+john-victor394@users.noreply.github.com>
2024-08-23 19:14:10 +0530

    Create Currency_converter.py

---
+++ Currency_converter.py
@@ -0,0 +1,8 @@
+pesos = int(input('Left in pesos'))
+soles= int(input('Left in soles'))
+reais = int(input('Left in reais'))
+uspe = pesos * 0.00025
+ussol= soles * 0.27 
+usrea = reais * 0.18
+Total = uspe + ussol + usrea
+print('total',Total)
